6ceb37a044ffd3764df91823d49f15c4f9c1f714
siwamkumarsingh <siwamsingh2003@gmail.com>
2024-02-26 23:42:24 +0530

    code: added PostForm

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
- function Logo({width = '100px'}) {
-  return (
-    <div >
-      Logo
-    </div>
-  )
+function Logo({ width = '100px' }) {
+    return (
+        <div >
+            Logo
+        </div>
+    )
 }
 
 export default Logo
